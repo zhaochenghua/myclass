19b3d8c4f33acd3b83470632e1bb00b6ff97936d
zhaochenghua <37543439+zhaochenghua@users.noreply.github.com>
2026-09-03 22:08:58 +0800

    chore: versions.json 纳入 git 自动同步，构建脚本自动复制 apk 到 public

--- android/AndroidStudioProject/build_apk.bat
+++ android/AndroidStudioProject/build_apk.bat
@@ -4,4 +4,5 @@ set ANDROID_HOME=C:\Users\zch\.local\android-sdk
 set GRADLE_USER_HOME=C:\Users\zch\.gradle
 cd /d C:\Users\zch\Documents\code\myclass\android\AndroidStudioProject
 call C:\Users\zch\.local\gradle-8.9\bin\gradle.bat assembleRelease
+copy /Y "app\build\outputs\apk\release\app-release.apk" "..\..\web\public\myclass.apk"
 node update-apk-version.js
